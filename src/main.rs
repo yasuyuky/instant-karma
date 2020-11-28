b0fee11b44d3ebb7fa6a6b77c31eef81aec011a5
yasuyuky <yasuyuki.ymd@gmail.com>
2020-11-28 22:02:43 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::sync::Mutex;
+use tide::Request;
+use uuid::Uuid;
+
+static mut GLOBAL_DATA: Lazy<Mutex<HashMap<u128, String>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    let mut buf = String::new();
+    let mut stdin = std::io::stdin();
+    stdin.read_to_string(&mut buf)?;
+    println!("{}", create(&buf));
+    app.at("/:id").get(get);
+    app.listen("127.0.0.1:4989").await?;
+    Ok(())
+}
+
+fn create(v: &str) -> String {
+    let k = Uuid::new_v4();
+    unsafe {
+        match GLOBAL_DATA.get_mut() {
+            Ok(d) => {
+                d.insert(k.as_u128(), String::from(v));
+            }
+            Err(_) => (),
+        }
+    }
+    format!("{}", k)
+}
+
+async fn get(req: Request<()>) -> tide::Result {
+    let k = Uuid::parse_str(req.param("id")?)?;
+    unsafe {
+        match GLOBAL_DATA.get_mut()?.get(&k.as_u128()) {
+            Some(s) => Ok(format!("{}", s).into()),
+            None => Ok(tide::Response::new(404)),
+        }
+    }
+}
