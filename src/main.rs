d446be006927b021b85c4ceacd6a47ac8f913c51
yasuyuky <yasuyuki.ymd@gmail.com>
2020-12-20 12:13:53 +0900

    Add the view function

--- src/main.rs
+++ src/main.rs
@@ -118,3 +118,22 @@ async fn get_copy(req: Request<()>) -> tide::Result {
         }
     }
 }
+
+async fn view(path: &Path) -> tide::Result<()> {
+    let pathstr = path.to_str().unwrap_or_default().to_owned();
+    let k = Uuid::new_v4();
+    println!("{}{}", CONFIG.prefix, k);
+    for entry in path.read_dir().expect("read dir") {
+        if let Ok(e) = entry {
+            let f = e.path().file_name().unwrap_or_default().to_owned();
+            println!("{}{}/{}", CONFIG.prefix, k, f.to_str().unwrap_or_default());
+        }
+    }
+    let app = async {
+        let mut app = tide::new();
+        app.at(&format!("/{}", k)).serve_dir(pathstr + "/")?;
+        app.listen("127.0.0.1:4989").await
+    };
+    app.race(ctrlc()).await?;
+    Ok(())
+}
