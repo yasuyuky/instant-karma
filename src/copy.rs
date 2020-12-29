d6aab177e4592801207688d6b337c3e20f168620
yasuyuky <yasuyuki.ymd@gmail.com>
2020-12-29 10:53:52 +0900

    Refactor copy

---
+++ src/copy.rs
@@ -0,0 +1,51 @@
+use async_std::prelude::*;
+use statics::*;
+use std::io::prelude::*;
+use tide::{http::mime, Request, Response};
+use uuid::Uuid;
+
+use crate::ctrlc;
+use crate::statics;
+
+pub async fn copy() -> tide::Result<()> {
+    let mut buf = String::new();
+    let mut stdin = std::io::stdin();
+    stdin.read_to_string(&mut buf)?;
+    let k = Uuid::new_v4();
+    put_dict(k.as_u128(), &buf);
+    println!("{}{}", CONFIG.prefix, k);
+    let app = async {
+        let mut app = tide::new();
+        app.at("/:id").get(get_copy);
+        app.listen(LISTENER.to_owned()).await
+    };
+    app.race(ctrlc()).await?;
+    Ok(())
+}
+
+fn put_dict(k: u128, v: &str) {
+    unsafe {
+        match GLOBAL_DATA.get_mut() {
+            Ok(d) => {
+                d.insert(k, v.to_owned());
+            }
+            Err(_) => (),
+        }
+    }
+}
+
+async fn get_copy(req: Request<()>) -> tide::Result {
+    let k = Uuid::parse_str(req.param("id")?)?;
+    unsafe {
+        match GLOBAL_DATA.get_mut()?.get(&k.as_u128()) {
+            Some(s) => {
+                let resp = COPY_TEMPLATE.replace("{}", &html_escape::encode_text(s));
+                Ok(Response::builder(200)
+                    .body(resp)
+                    .content_type(mime::HTML)
+                    .build())
+            }
+            None => Ok(tide::Response::new(404)),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-use tide::{http::mime, Request, Response};
 use uuid::Uuid;
 
+mod copy;
 mod statics;
 use statics::*;
 
@@ -54,7 +54,7 @@ impl Config {
 async fn main() -> tide::Result<()> {
     let opt = Opt::from_args();
     match opt.cmd {
-        Command::Copy => copy().await?,
+        Command::Copy => copy::copy().await?,
         Command::View { path } => view(&path).await?,
     }
     Ok(())
@@ -64,49 +64,6 @@ async fn ctrlc() -> Result<(), std::io::Error> {
     CtrlC::new().expect("Cannot use CTRL-C handler").await;
     println!("termination signal received, stopping server...");
     Ok(())
-}
-
-pub async fn copy() -> tide::Result<()> {
-    let mut buf = String::new();
-    let mut stdin = std::io::stdin();
-    stdin.read_to_string(&mut buf)?;
-    let k = Uuid::new_v4();
-    put_dict(k.as_u128(), &buf);
-    println!("{}{}", CONFIG.prefix, k);
-    let app = async {
-        let mut app = tide::new();
-        app.at("/:id").get(get_copy);
-        app.listen(LISTENER.to_owned()).await
-    };
-    app.race(ctrlc()).await?;
-    Ok(())
-}
-
-fn put_dict(k: u128, v: &str) {
-    unsafe {
-        match GLOBAL_DATA.get_mut() {
-            Ok(d) => {
-                d.insert(k, v.to_owned());
-            }
-            Err(_) => (),
-        }
-    }
-}
-
-async fn get_copy(req: Request<()>) -> tide::Result {
-    let k = Uuid::parse_str(req.param("id")?)?;
-    unsafe {
-        match GLOBAL_DATA.get_mut()?.get(&k.as_u128()) {
-            Some(s) => {
-                let resp = COPY_TEMPLATE.replace("{}", &html_escape::encode_text(s));
-                Ok(Response::builder(200)
-                    .body(resp)
-                    .content_type(mime::HTML)
-                    .build())
-            }
-            None => Ok(tide::Response::new(404)),
-        }
-    }
 }
 
 async fn view(path: &Path) -> tide::Result<()> {
