18a661c4c0e49fb5ddac2ae9836f649f9e29e156
yasuyuky <yasuyuki.ymd@gmail.com>
2020-12-30 01:22:57 +0900

    Return item list as Entry

--- src/view.rs
+++ src/view.rs
@@ -9,25 +9,20 @@ use crate::statics;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Entry {
-    File {
-        path: PathBuf,
-    },
     Dir {
         path: PathBuf,
         children: BTreeSet<Entry>,
+    },
+    File {
+        path: PathBuf,
     },
 }
 
 pub async fn view(path: &Path) -> tide::Result<()> {
     let k = Uuid::new_v4();
     println!("{}{}", CONFIG.prefix, k);
-    let items = list_items(&path).unwrap_or_default();
-    for e in items {
-        if let Entry::File { path: p } = e {
-            let rp = p.strip_prefix(path).unwrap();
-            println!("{}{}/{}", CONFIG.prefix, k, rp.to_str().unwrap_or_default());
-        }
-    }
+    let root = list_items(&path)?;
+    print_recursively(path, k, root);
     let app = async {
         let mut app = tide::new();
         app.at(&format!("/{}", k)).serve_dir(&path)?;
@@ -37,7 +32,7 @@ pub async fn view(path: &Path) -> tide::Result<()> {
     Ok(())
 }
 
-fn list_items(path: &Path) -> Result<BTreeSet<Entry>, std::io::Error> {
+fn list_items(path: &Path) -> Result<Entry, std::io::Error> {
     let mut result = BTreeSet::new();
     for entry in path.read_dir().expect("read dir") {
         if let Ok(e) = entry {
@@ -46,14 +41,26 @@ fn list_items(path: &Path) -> Result<BTreeSet<Entry>, std::io::Error> {
                     path: PathBuf::from(e.path()),
                 });
             } else if e.metadata()?.is_dir() {
-                let children = list_items(&e.path())?;
-                result.insert(Entry::Dir {
-                    path: PathBuf::from(e.path()),
-                    children: children.clone(),
-                });
-                result.extend(children);
+                result.insert(list_items(&e.path())?);
             }
         }
     }
-    Ok(result)
+    Ok(Entry::Dir {
+        path: PathBuf::from(path),
+        children: result,
+    })
+}
+
+fn print_recursively(base: &Path, k: Uuid, root: Entry) {
+    match root {
+        Entry::File { path: p } => {
+            let rp = p.strip_prefix(base).unwrap();
+            println!("{}{}/{}", CONFIG.prefix, k, rp.to_str().unwrap_or_default());
+        }
+        Entry::Dir { path: _, children } => {
+            for e in children {
+                print_recursively(base, k, e)
+            }
+        }
+    }
 }
