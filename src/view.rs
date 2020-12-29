d7ce4f7d618066edbc2d3db420c23780e547bfa2
yasuyuky <yasuyuki.ymd@gmail.com>
2020-12-29 21:47:37 +0900

    Add the Entry enum

--- src/view.rs
+++ src/view.rs
@@ -7,12 +7,26 @@ use uuid::Uuid;
 use crate::ctrlc;
 use crate::statics;
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Entry {
+    File {
+        path: PathBuf,
+    },
+    Dir {
+        path: PathBuf,
+        children: BTreeSet<Entry>,
+    },
+}
+
 pub async fn view(path: &Path) -> tide::Result<()> {
     let k = Uuid::new_v4();
     println!("{}{}", CONFIG.prefix, k);
-    for p in list_items(&path).unwrap_or_default() {
-        let rp = p.strip_prefix(path).unwrap();
-        println!("{}{}/{}", CONFIG.prefix, k, rp.to_str().unwrap_or_default());
+    let items = list_items(&path).unwrap_or_default();
+    for e in items {
+        if let Entry::File { path: p } = e {
+            let rp = p.strip_prefix(path).unwrap();
+            println!("{}{}/{}", CONFIG.prefix, k, rp.to_str().unwrap_or_default());
+        }
     }
     let app = async {
         let mut app = tide::new();
@@ -23,14 +37,21 @@ pub async fn view(path: &Path) -> tide::Result<()> {
     Ok(())
 }
 
-fn list_items(path: &Path) -> Result<BTreeSet<PathBuf>, std::io::Error> {
+fn list_items(path: &Path) -> Result<BTreeSet<Entry>, std::io::Error> {
     let mut result = BTreeSet::new();
     for entry in path.read_dir().expect("read dir") {
         if let Ok(e) = entry {
             if e.metadata()?.is_file() {
-                result.insert(PathBuf::from(e.path()));
+                result.insert(Entry::File {
+                    path: PathBuf::from(e.path()),
+                });
             } else if e.metadata()?.is_dir() {
-                result.extend(list_items(&e.path())?);
+                let children = list_items(&e.path())?;
+                result.insert(Entry::Dir {
+                    path: PathBuf::from(e.path()),
+                    children: children.clone(),
+                });
+                result.extend(children);
             }
         }
     }
