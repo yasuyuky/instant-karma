84e93c8c99e6b079417063fdc8209b9315713a4e
yasuyuky <yasuyuki.ymd@gmail.com>
2021-02-20 23:43:46 +0900

    Add the function to load file

--- src/statics.rs
+++ src/statics.rs
@@ -2,7 +2,7 @@ use crate::config::Config;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use std::io::{stdin, Read};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Mutex;
 use uuid::Uuid;
 
@@ -42,6 +42,15 @@ pub fn load_stdin_to_dict() -> Result<Uuid, std::io::Error> {
     let mut buf = String::new();
     let mut stdin = stdin();
     stdin.read_to_string(&mut buf)?;
+    let k = Uuid::new_v4();
+    put_dict(k.as_u128(), &buf);
+    Ok(k)
+}
+
+pub fn load_file_to_dict(path: &Path) -> Result<Uuid, std::io::Error> {
+    let mut buf = String::new();
+    let mut f = std::fs::File::open(path)?;
+    f.read_to_string(&mut buf)?;
     let k = Uuid::new_v4();
     put_dict(k.as_u128(), &buf);
     Ok(k)
