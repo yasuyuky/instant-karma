e0d544c85268adca381adf557e1a2ac38bd7f5ca
yasuyuky <yasuyuki.ymd@gmail.com>
2021-03-07 21:38:34 +0900

    Remove an unused import

--- src/render.rs
+++ src/render.rs
@@ -2,7 +2,7 @@ use crate::ctrlc;
 use crate::key::Key;
 use crate::load::load_input_to_dict;
 use crate::statics::*;
-use crate::watch::{handle_sse_req, load_path, watch_path};
+use crate::watch::{handle_sse_req, watch_path};
 use async_std::prelude::*;
 use pulldown_cmark::{html, Options, Parser};
 use std::path::PathBuf;
